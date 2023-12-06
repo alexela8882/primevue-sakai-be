cfb54e7a41192aca3224419a004d13d65545e888
alexela8882 <alexela8882@gmail.com>
2023-12-06 16:41:14 +0800

    re-config passport settings for production

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,12 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,
+        nav.reddotcrm.com,
+        localhost:3000,
+        127.0.0.1,
+        127.0.0.1:8000,
+        ::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
