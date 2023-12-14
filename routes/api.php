bd2bf6291ce841b3a9479b2440d77dad1dd37f63
alexela8882 <alexela8882@gmail.com>
2023-12-14 10:20:47 +0800

    saml2 updates

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,11 @@ Route::controller(BranchController::class)
 });
 
 // SAML2 Auth
-// Route::prefix('saml2-auth')
-//   ->group(function () {
-//   Route::get
-// });
+Route::prefix('saml2-auth')->group(function () {
+  Route::get('login', function () {
+    return redirect()->route('saml.login', ['uuid' => '07766233-180f-4f62-ad22-9f16ff83e4c9']);
+  });
+  Route::get('logout', function () {
+    return redirect()->route('saml.logout', ['uuid' => '07766233-180f-4f62-ad22-9f16ff83e4c9']);
+  });
+});
