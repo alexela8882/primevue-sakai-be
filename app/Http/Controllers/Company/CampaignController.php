3685632e2d12dbd36f92b66075c9c2968847bee8
Eljohn Duñgo <dungo.eljohn@gmail.com>
2024-02-28 08:46:59 +0800

    campaign index and show

--- app/Http/Controllers/Company/CampaignController.php
+++ app/Http/Controllers/Company/CampaignController.php
@@ -9,14 +9,20 @@ use Illuminate\Http\Request;
 
 class CampaignController extends Controller
 {
+	
+	protected $user;
+
 	public function __construct(private ModuleDataCollector $moduleDataCollector)
     {
         $this->moduleDataCollector = $moduleDataCollector->setUser()->setModule('campaigns');
+
+		$this->user = auth('api')->user();
     }
 
 	public function index(Request $request)
     {
-        return $this->moduleDataCollector->getIndex($request);
+        if($this->user->canView('campaigns'))
+			return $this->moduleDataCollector->getIndex($request);
     }
 
 	public function show(Campaign $campaign, Request $request)
