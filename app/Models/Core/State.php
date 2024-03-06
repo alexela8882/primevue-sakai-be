ee9b05a7e705b8d23d53001e6136ea5473af265e
cha eu <leviitee1611@gmail.com>
2024-03-06 16:12:22 +0800

    update

---
+++ app/Models/Core/State.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Core;
+
+use App\Models\Model\Base;
+
+class State extends Base
+{
+    protected $entity = 'State';
+    protected $collection = 'states';
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+}
