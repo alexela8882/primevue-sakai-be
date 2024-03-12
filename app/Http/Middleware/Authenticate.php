3ff7f20ae3700e1a4e734f8bc9b6b122fb5b3d6f
Hobert Mejia <hobert.mejia14@gmail.com>
2024-03-12 18:45:50 +0800

    Added pricebook

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,14 +4,19 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(Response::json([
+            'message' => 'Error. Unauthenticated user. Kindly login first.',
+        ], 401));
     }
 }
