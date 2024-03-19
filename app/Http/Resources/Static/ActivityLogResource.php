ac966723fa1d5c441ca5f83c648690a41e824ef0
Alexander Flores <alexela8882@gmail.com>
2024-03-19 16:18:09 +0800

    ActivityLog model, resource and controller created; routes api updated: activity log route added

---
+++ app/Http/Resources/Static/ActivityLogResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\Static;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+
+class ViewFilterResource extends JsonResource
+{
+    public static function customItemCollection($item) {
+      return $item;
+    }
+
+    public static function customCollection($resource) {
+      return $resource;
+    }
+}
---
+++ app/Models/Static/ActivityLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Static;
+
+use App\Models\Model\Base;
+
+class ActivityLog extends Base
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'activity_logs';
+}
