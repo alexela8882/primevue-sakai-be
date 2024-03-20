e6a64038749fd938c7ecc4d888708c6984f283a1
Alexander Flores <alexela8882@gmail.com>
2024-03-20 11:47:53 +0800

    ActivityLogController updates: link_id renamed to record_id, log_type field added, response structured, getting index by record method created

--- app/Http/Controllers/Static/ActivityLogController.php
+++ app/Http/Controllers/Static/ActivityLogController.php
@@ -20,30 +20,30 @@ class ActivityLogController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        $rules = [
-            'module_id' => 'required',
-            'link_id' => 'required',
-            'subject' => 'required',
-            'type_id' => 'required',
-            'date' => 'required',
-            'status' => 'required',
-            'remarks' => 'required',
-        ];
-        $messages = [
-            'required' => 'The :attribute field is required.',
-            'unique' => 'This :attribute has already been taken',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
+    public function store(Request $request) {
+      $rules = [
+        'module_id' => 'required',
+        'record_id' => 'required',
+        'subject' => 'required',
+        'type_id' => 'required',
+        'date' => 'required',
+        'status' => 'required',
+        'remarks' => 'required',
+      ];
+      $messages = [
+        'required' => 'The :attribute field is required.',
+        'unique' => 'This :attribute has already been taken',
+      ];
+      $validator = Validator::make($request->all(), $rules, $messages);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
+      if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
+      }
 
       $log = new ActivityLog;
       $log->module_id = $request->module_id;
-      $log->link_id = $request->link_id;
+      $log->record_id = $request->record_id;
+      $log->log_type = $request->log_type;
       $log->subject = $request->subject;
       $log->type_id = $request->type_id;
       $log->date = $request->date;
@@ -52,13 +52,13 @@ class ActivityLogController extends Controller
       $log->created_by = auth()->user()->id;
       $log->save();
 
-        $response = [
-            'data' => $log,
-            'message' => 'New log was successfully added.',
-            'status' => 200,
-        ];
+      $response = [
+        'data' => $log,
+        'message' => 'New log was successfully added.',
+        'status' => 200
+      ];
 
-        return response()->json($response, $response['status']);
+      return response()->json($response, $response['status']);
     }
 
     /**
@@ -67,6 +67,35 @@ class ActivityLogController extends Controller
     public function show(ActivityLog $activityLog)
     {
         //
+    }
+
+    /**
+     * Display the specified resource by record_id field.
+     */
+    public function indexByRecord ($record_id) {
+      $logs = ActivityLog::where('record_id', $record_id)->get();
+
+      try {
+        if (count($logs) > 0) {
+          $message = 'Activity logs by record successfully fetched.';
+          $status = 200;
+        } else {
+          $message = 'No records found';
+          $status = 200;
+        }
+      } catch (\Throwable $th) {
+        $logs = [];
+        $message = 'Error fetching data';
+        $status = 500;
+      }
+
+      $response = [
+        'data' => $logs,
+        'message' => $message,
+        'status' => $status
+      ];
+
+      return response()->json($response, $status);
     }
 
     /**
